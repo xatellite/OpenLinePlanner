3bd54ff3acb2d52452bb31e4990f0907b30ff8dd
David Prenninger <david.prenninger@gmx.de>
2022-05-27 09:07:35 +0200

    Fixing Geopackage append

--- openlineplanner_backend/main.py
+++ openlineplanner_backend/main.py
@@ -14,7 +14,7 @@ def load_gdf(path):
   gdf = geopandas.read_file(path + "/" + files[0], layer= files[0][:-5])
   for file_index in range(1, len(files)):
     gdf_new = geopandas.read_file(path + "/" + files[file_index], layer=files[file_index][:-5])
-    gdf.append(gdf_new)
+    gdf = gdf.append(gdf_new, ignore_index=True)
   return gdf
 
 
