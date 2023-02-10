739c7e16b4c595f2c774cb434508116008e46e91
Maximilian Zander <maximilian.zander@zmnet.de>
2023-02-10 19:26:24 +0100

    commit missing file

--- openlineplanner-rs/src/station.rs
+++ openlineplanner-rs/src/station.rs
@@ -1,5 +1,10 @@
-use geo::{HaversineDistance, Point};
+use std::borrow::Borrow;
+
+use actix_web::{body::BoxBody, http::header::ContentType, HttpResponse, Responder};
+use geo::{Densify, HaversineDistance, LineString, Point};
 use serde::Deserialize;
+
+use crate::{coverage::get_houses_in_coverage, coverage::StationCoverageInfo, datalayer::House};
 
 static DEFAULT_COVERAGE: f64 = 300f64;
 
@@ -17,5 +22,32 @@ impl Station {
 
     pub fn coverage(&self) -> f64 {
         self.coverage.unwrap_or(DEFAULT_COVERAGE)
+    }
+}
+
+pub fn find_optimal_station(
+    line: Vec<Point>,
+    coverage: f64,
+    houses: &[House],
+    other_stations: &[Station],
+) -> OptimalStationResult {
+    let linestring = Into::<LineString>::into(line).densify(10f64);
+    let others: Vec<&Station> = other_stations.iter().map(|x| x.borrow()).collect();
+    OptimalStationResult(linestring.points().max_by_key(|point| {
+        // TODO is max_by_key use like this efficient?
+        StationCoverageInfo::from(get_houses_in_coverage(&point, coverage, houses, &others))
+            .inhabitants
+    }))
+}
+
+pub struct OptimalStationResult(Option<Point>);
+
+impl Responder for OptimalStationResult {
+    type Body = BoxBody;
+
+    fn respond_to(self, _req: &actix_web::HttpRequest) -> actix_web::HttpResponse<Self::Body> {
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .json(self.0)
     }
 }
