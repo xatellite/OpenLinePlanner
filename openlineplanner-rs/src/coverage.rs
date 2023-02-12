5ed46891957141be1f80cd71914d2b47e2b0af11
zandemax <hi@zdmx.me>
2023-02-12 18:44:14 +0100

    Add primitive osm router

--- openlineplanner-rs/src/coverage.rs
+++ openlineplanner-rs/src/coverage.rs
@@ -1,4 +1,8 @@
+use geo::HaversineDistance;
 use geo::Point;
+use osmpbfreader::NodeId;
+use petgraph::algo::dijkstra;
+use petgraph::prelude::UnGraphMap;
 use serde::Deserialize;
 use serde::Serialize;
 
@@ -14,6 +18,12 @@ pub struct CoverageMap<'a, 'b>(pub HashMap<&'a str, StationCoverageInfo<'b>>);
 pub struct StationCoverageInfo<'a> {
     pub houses: Vec<HouseInfo<'a>>,
     pub inhabitants: u32,
+}
+
+#[derive(Deserialize)]
+pub enum Distance {
+    Naive,
+    Osm,
 }
 
 impl<'a> StationCoverageInfo<'a> {
@@ -72,10 +82,50 @@ pub fn get_houses_in_coverage<'a>(
         .collect()
 }
 
+pub fn get_houses_in_coverage_with_real_distance<'a>(
+    origin: &Point,
+    coverage: f64,
+    houses: &'a [House],
+    possible_collision_stations: &[&Station],
+    nodes: &HashMap<NodeId, Point>,
+    streetgraph: &UnGraphMap<NodeId, f64>,
+) -> Vec<HouseInfo<'a>> {
+    let (origin_node, diff_distance) = find_closest_node_to_point(nodes, origin);
+    let osm_distance_matrix = dijkstra(streetgraph, origin_node, None, |e| *e.2);
+    houses
+        .iter() //TODO: Restructure that filter_map
+        .filter_map(|house| {
+            if house.street_graph_id.is_none() || osm_distance_matrix.get(&house.street_graph_id.unwrap()).unwrap_or(&f64::MAX) + diff_distance > coverage {
+                None
+            } else {
+                Some(HouseInfo{house, distance: osm_distance_matrix.get(&house.street_graph_id.unwrap()).unwrap() + diff_distance})
+            }
+        }) // House is in the radius of our station
+        .filter(|hi| {
+            possible_collision_stations.iter().all(|other| {
+                hi.house.haversine_distance(&other.location) > other.coverage() // House is not in the coverage area of the other station or
+                    || hi.house.haversine_distance(&origin) < hi.house.haversine_distance(&other.location)
+                // House is closer to the current station
+            })
+        })
+        .collect()
+}
+
+fn find_closest_node_to_point(nodes: &HashMap<NodeId, Point>, origin: &Point) -> (NodeId, f64) {
+    nodes
+        .iter()
+        .min_by_key(|(_, node)| node.haversine_distance(&origin) as u32)
+        .map(|(id, node)| (id.clone(), node.haversine_distance(&origin)))
+        .unwrap()
+}
+
 pub fn houses_for_stations<'a, 'b>(
     stations: &'a [Station],
     houses: &'b [House],
     method: &Method,
+    distance: &Distance,
+    nodes: &HashMap<NodeId, Point>,
+    streetgraph: &UnGraphMap<NodeId, f64>,
 ) -> CoverageMap<'a, 'b> {
     let mut inhabitants_map = HashMap::new();
 
@@ -87,12 +137,22 @@ pub fn houses_for_stations<'a, 'b>(
                 other.haversine_distance(station) < (other.coverage() + station.coverage())
             })
             .collect();
-        let houses = get_houses_in_coverage(
-            &station.location,
-            station.coverage(),
-            houses,
-            &possible_collision_stations,
-        );
+        let houses = match distance {
+            Distance::Naive => get_houses_in_coverage(
+                &station.location,
+                station.coverage(),
+                houses,
+                &possible_collision_stations,
+            ),
+            Distance::Osm => get_houses_in_coverage_with_real_distance(
+                &station.location,
+                station.coverage(),
+                houses,
+                &possible_collision_stations,
+                nodes,
+                streetgraph,
+            ),
+        };
         inhabitants_map.insert(
             station.id.as_str(),
             StationCoverageInfo::from_houses_with_method(houses, method),
--- openlineplanner-rs/src/datalayer.rs
+++ openlineplanner-rs/src/datalayer.rs
@@ -1,10 +1,17 @@
+use std::collections::HashMap;
 use std::fs::File;
+use std::path::Path;
+use std::path::PathBuf;
 
 use actix_web::body::BoxBody;
 use actix_web::http::header::ContentType;
 use actix_web::HttpResponse;
 use actix_web::Responder;
+use geo::Closest;
+use geo::ClosestPoint;
 use geo::HaversineDistance;
+use geo::HaversineLength;
+use geo::Line;
 use geo::Point;
 
 use anyhow::Result;
@@ -12,6 +19,9 @@ use geojson::de::deserialize_feature_collection_to_vec;
 use geojson::de::deserialize_geometry;
 use geojson::ser::serialize_geometry;
 use geojson::ser::to_feature_collection_string;
+use osmpbfreader::NodeId;
+use osmpbfreader::OsmObj;
+use petgraph::prelude::UnGraphMap;
 use serde::Deserialize;
 use serde::Serialize;
 
@@ -27,10 +37,13 @@ pub enum DataLayerName {
     Jobs,
 }
 
+#[derive(Clone)]
 pub struct DataLayers {
     pub residence: DataLayer,
     pub schools: DataLayer,
     pub jobs: DataLayer,
+    pub streetgraph: UnGraphMap<NodeId, f64>,
+    pub nodes: HashMap<NodeId, Point>,
 }
 
 #[derive(Serialize, Deserialize, Clone)]
@@ -43,6 +56,7 @@ pub struct House {
     flats: u32,
     housenumbers: u32,
     pub pop: u32,
+    pub street_graph_id: Option<NodeId>,
 }
 
 impl House {
@@ -137,11 +151,17 @@ pub struct DataFilePaths {
     residence: String,
     schools: String,
     jobs: String,
+    osm: String,
 }
 
 pub fn load_data_layer_files(paths: DataFilePaths) -> Result<DataLayers> {
     let residence_file = File::open(paths.residence)?;
-    let residence = DataLayer(deserialize_feature_collection_to_vec(residence_file)?);
+    let residence_raw = deserialize_feature_collection_to_vec(residence_file)?;
+
+    let (streetgraph, residence_with_streetconnection, nodes) =
+        read_osm_nodes(&PathBuf::from(paths.osm), residence_raw)?;
+
+    let residence = DataLayer(residence_with_streetconnection);
 
     let schools_file = File::open(paths.schools)?;
     let schools = DataLayer(deserialize_feature_collection_to_vec(schools_file)?);
@@ -153,5 +173,128 @@ pub fn load_data_layer_files(paths: DataFilePaths) -> Result<DataLayers> {
         residence,
         schools,
         jobs,
+        streetgraph,
+        nodes,
     })
+}
+
+pub fn read_osm_nodes(
+    file: &Path,
+    mut houses: Vec<House>,
+) -> Result<(UnGraphMap<NodeId, f64>, Vec<House>, HashMap<NodeId, Point>)> {
+    let r = std::fs::File::open(file)?;
+    let mut pbf = osmpbfreader::OsmPbfReader::new(r);
+
+    let osm_nodes = pbf.get_objs_and_deps(|obj| obj.is_node()).unwrap();
+
+    let nodes: HashMap<NodeId, Point> = osm_nodes
+        .into_iter()
+        .filter_map(|(_, obj)| match obj {
+            OsmObj::Node(inner) => Some(inner),
+            _ => None,
+        })
+        .map(|node| {
+            (
+                node.id,
+                Point::new(
+                    (node.decimicro_lon as f64 * 1e-7) as f64,
+                    (node.decimicro_lat as f64 * 1e-7) as f64,
+                ),
+            )
+        })
+        .collect();
+
+    let osm_ways = pbf
+        .get_objs_and_deps(|obj| obj.is_way() && obj.tags().contains_key("highway"))
+        .unwrap();
+
+    let mut edges: Vec<(NodeId, NodeId, (Line, f64))> = osm_ways
+        .into_iter()
+        .filter_map(|(_, obj)| match obj {
+            OsmObj::Way(inner) => Some(inner),
+            _ => None,
+        })
+        .filter(|way| way.nodes.len() >= 2)
+        .map(|way| {
+            let mut other = way.nodes.clone().into_iter();
+            other.next();
+            way.nodes.into_iter().zip(other)
+        })
+        .flatten()
+        .map(|(n1, n2)| {
+            let line = Line::new(
+                nodes.get(&n1).unwrap().to_owned(),
+                nodes.get(&n2).unwrap().to_owned(),
+            );
+            (n1, n2, (line, line.haversine_length()))
+        })
+        .collect();
+
+    let nodes: HashMap<NodeId, Point> = edges
+        .iter()
+        .map(|edge| vec![nodes.get_key_value(&edge.0), nodes.get_key_value(&edge.1)])
+        .flatten()
+        .flatten()
+        .map(|(a, b)| (a.clone(), b.clone()))
+        .collect();
+
+    println!("edges length: {}", edges.len());
+
+    for mut house in &mut houses {
+        let closest_street_point = edges
+            .iter()
+            .filter_map(|edge| {
+                let closest_point = edge.2 .0.closest_point(&house.geometry);
+                match closest_point {
+                    Closest::Intersection(point) | Closest::SinglePoint(point) => Some((
+                        point.haversine_distance(&house.geometry),
+                        point,
+                        edge.clone(),
+                    )),
+                    _ => None,
+                }
+            })
+            .min_by_key(|(distance, _, _)| *distance as u32);
+
+        if let Some((distance, point_n, old_edge)) = closest_street_point {
+            let (node_a, node_b, _) = old_edge;
+            let index = edges.iter().position(|elem| elem == &old_edge).unwrap();
+            edges.remove(index);
+            let node_n = get_new_node_id(&nodes);
+            if nodes.get(&node_a).is_none() {
+                continue;
+            };
+            let line_a_n = Line::new(nodes.get(&node_a).unwrap().to_owned(), point_n);
+            edges.push((node_a, node_n, (line_a_n, line_a_n.haversine_length())));
+            if nodes.get(&node_b).is_none() {
+                continue;
+            };
+            let line_n_b = Line::new(point_n, nodes.get(&node_b).unwrap().to_owned());
+            edges.push((node_n, node_b, (line_n_b, line_n_b.haversine_length())));
+            let node_h = get_new_node_id(&nodes);
+            let line_h_n = Line::new(house.geometry, point_n);
+            edges.push((node_h, node_n, (line_h_n, distance)));
+
+            house.street_graph_id = Some(node_h);
+        }
+    }
+
+    let edges: Vec<(NodeId, NodeId, f64)> = edges
+        .into_iter()
+        .map(|(a, b, (_, distance))| (a, b, distance))
+        .collect();
+
+    println!("edges length: {}", edges.len());
+
+    let graph = UnGraphMap::from_edges(edges);
+
+    Ok((graph, houses, nodes))
+}
+
+fn get_new_node_id(nodes: &HashMap<NodeId, Point>) -> NodeId {
+    let mut random_id = NodeId(rand::random());
+    while nodes.contains_key(&random_id) {
+        random_id = NodeId(rand::random());
+    }
+    random_id
 }
