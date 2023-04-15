2e9e69de65f7f8ab5ff58d41c52daa9aff79741d
Max <hi@zdmx.me>
2023-04-15 15:25:42 +0200

    fix formatting

--- openlineplanner-backend/src/layers/streetgraph.rs
+++ openlineplanner-backend/src/layers/streetgraph.rs
@@ -1,34 +1,45 @@
-use std::{collections::HashMap};
+use std::collections::HashMap;
 
 use geo::{HaversineDistance, Point};
 use osmpbfreader::{NodeId, OsmObj, OsmPbfReader};
 use petgraph::prelude::UnGraphMap;
-use serde::{Serialize, Deserialize};
-
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Streets {
     pub nodes: HashMap<NodeId, Point>,
-    #[serde(serialize_with = "Streets::serialize_streetgraph", deserialize_with = "Streets::deserialize_streetgraph")]
+    #[serde(
+        serialize_with = "Streets::serialize_streetgraph",
+        deserialize_with = "Streets::deserialize_streetgraph"
+    )]
     pub streetgraph: UnGraphMap<NodeId, f64>,
 }
 
 impl Streets {
-    fn serialize_streetgraph<S>(graph: &UnGraphMap<NodeId, f64>, serializer: S) -> Result<S::Ok, S::Error> where S: serde::Serializer {
-        let edges: Vec<(NodeId, NodeId, f64)> = graph.all_edges().into_iter().map(|edge| (edge.0, edge.1, *edge.2)).collect();
-       
+    fn serialize_streetgraph<S>(
+        graph: &UnGraphMap<NodeId, f64>,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let edges: Vec<(NodeId, NodeId, f64)> = graph
+            .all_edges()
+            .into_iter()
+            .map(|edge| (edge.0, edge.1, *edge.2))
+            .collect();
+
         Serialize::serialize(&edges, serializer)
     }
 
-    fn deserialize_streetgraph<'de, D>(deserializer: D) -> Result<UnGraphMap<NodeId, f64>, D::Error> 
+    fn deserialize_streetgraph<'de, D>(deserializer: D) -> Result<UnGraphMap<NodeId, f64>, D::Error>
     where
-        D: serde::Deserializer<'de> {
-        
+        D: serde::Deserializer<'de>,
+    {
         let edges: Vec<(NodeId, NodeId, f64)> = Deserialize::deserialize(deserializer)?;
 
         let streetgraph = UnGraphMap::from_edges(edges);
         Ok(streetgraph)
-
     }
 }
 
